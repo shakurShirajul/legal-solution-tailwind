40d4fe0a153570ce8aff947a59ab03f8c927ac97
shakurShirajul <siirajulisllam@gmail.com>
2024-01-21 10:25:15 +0600

    Set Up Project

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  content: ['**/*.html'],
+  darkMode: 'class',
+  theme: {
+    extend: {},
+  },
+  plugins: [require("daisyui")],
+  daisyui: {
+    darkTheme: "light"
+  },
+}
+
